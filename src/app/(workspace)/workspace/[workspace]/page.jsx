9a3e50144f50677822d9a9930aec25e4fba85fe7
mengsea <mrmengsea@gmail.com>
2025-04-04 13:20:50 +0700

    this code contain a lot of bugs :bug:

--- src/app/(workspace)/workspace/[workspace]/page.jsx
+++ src/app/(workspace)/workspace/[workspace]/page.jsx
@@ -1,7 +1,18 @@
 "use server";
 import CardComponent from "@/components/card";
 import { todoService } from "@/service/todoService";
-
+import { Button } from "@/components/ui/button";
+import { SquareCheckBig } from "lucide-react";
+import {
+     Dialog,
+     DialogContent,
+     DialogDescription,
+     DialogFooter,
+     DialogHeader,
+     DialogTitle,
+     DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
 const WorkSpace = async ({ params }) => {
      const id = params.workspace;
      const res = await todoService(id);
@@ -30,6 +41,42 @@ const WorkSpace = async ({ params }) => {
                          <h2 className="border-b gap-y-2 border-green-500 text-green-500 text-xl">Finshed</h2>
                          {/* <CardComponent /> */}
                     </div>
+               </div>
+               <div className="fixed bottom-5 right-5">
+                    <Dialog>
+                         <DialogTrigger asChild>
+                              <Button variant="ghost" className="bg-blue-500 text-white p-4 rounded-full">
+                                   <SquareCheckBig /> Add New Workspace
+                              </Button>
+                         </DialogTrigger>
+                         <DialogContent className="sm:max-w-[425px] bg-white">
+                              <DialogHeader>
+                                   <DialogTitle>Create New Task</DialogTitle>
+                              </DialogHeader>
+                              <form className="grid gap-4 py-4">
+                                   <div className="space-y-2">
+                                        <Input
+                                             id="Title"
+                                             name="title"
+                                             placeholder="Insert you gaol title"
+                                             className="w-full"
+                                        />
+                                        <Input
+                                             id="newWorkspace"
+                                             name="tag"
+                                             placeholder="Insert your note here"
+                                             className="w-full"
+                                        />
+                                   </div>
+
+                                   <DialogFooter>
+                                        <Button type="submit" className={`text-blue-400 px-7 py-3 border rounded-xl`}>
+                                             Create
+                                        </Button>
+                                   </DialogFooter>
+                              </form>
+                         </DialogContent>
+                    </Dialog>
                </div>
           </div>
      );
